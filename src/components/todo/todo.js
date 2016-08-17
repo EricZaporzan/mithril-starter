f703aeed23b5229dfc72e2e3c022c537fef547d7
Brandon Martel <brandonmartel@gmail.com>
2016-08-16 19:19:10 -0500

    fix completed filter

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -4,6 +4,7 @@ import TodoVM from '../../models/todo';
 const todoVM = new TodoVM();
 
 const completed = todo => (todo.completed() ? 'completed' : '');
+const filterCompleted = todo => !(todoVM.hideCompleted() && todo.completed());
 
 const Todo = {
   oninit() {
@@ -13,7 +14,6 @@ const Todo = {
 
   view() {
     const remaining = this.vm.remaining();
-    const hideCompleted = this.vm.hideCompleted();
 
     return m('.todo', [
       m('h2', 'Todo List'),
@@ -28,16 +28,16 @@ const Todo = {
         },
       }, 'Add'),
       m('button.toggle-completed', {
-        class: hideCompleted ? 'hidden' : '',
+        class: this.vm.hideCompleted() ? 'hidden' : '',
         onclick: () => this.vm.hideCompleted(!this.vm.hideCompleted()),
       }, 'Hide Completed'),
-      this.vm.todos.length === 0 ? m('p', 'No todos :(. Add some!') : m('.todo-list', [
-        remaining ? m('h4', `Remaining todos ${remaining}`) : null,
-        this.vm.todos.map((todo, index) => hideCompleted && todo.completed() ? null : m('.todo-item', [
+      remaining ? m('h4', `Remaining todos ${remaining}`) : null,
+      this.vm.todos.length === 0 ? m('p', 'No todos :(. Add some!') : m('.todo-list',
+        this.vm.todos.filter(filterCompleted).map((todo, index) => m('.todo-item', [
           m('span', { class: completed(todo), onclick: () => todo.toggleComplete() }, todo.title()),
           m('button', { onclick: () => this.vm.remove(index) }, 'x'),
         ])),
-      ]),
+      ),
     ]);
   },
 };
